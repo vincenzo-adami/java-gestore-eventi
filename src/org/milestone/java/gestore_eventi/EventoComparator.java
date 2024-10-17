c8d67df5c6f39217a55f0d8785484960b5bb12fb
vincenzo-adami <51445073+vincenzo-adami@users.noreply.github.com>
2024-10-17 09:44:47 +0200

    ADD: titolo come criterio di comparazione

--- src/org/milestone/java/gestore_eventi/EventoComparator.java
+++ src/org/milestone/java/gestore_eventi/EventoComparator.java
@@ -6,10 +6,10 @@ public class EventoComparator implements Comparator<Evento> {
 
   @Override
   public int compare(Evento e1, Evento e2) {
-    if (e1.getData().isBefore(e2.getData())) {
+    if (e1.getData().isBefore(e2.getData()) && (e1.getTitolo().compareToIgnoreCase(e2.getTitolo()) < 0)) {
       return -1;
     }
-    if (e1.getData().isAfter(e2.getData())) {
+    if (e1.getData().isAfter(e2.getData()) && (e1.getTitolo().compareToIgnoreCase(e2.getTitolo()) > 0)) {
       return 1;
     }
     return 0;
